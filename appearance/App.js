b38c0138bd79bfadd97e1155d6def9a51e4a7b59
chinmayamurari <61820068+chinmayamurari@users.noreply.github.com>
2020-03-20 11:50:53 +0530

    update app.js

--- appearance/App.js
+++ appearance/App.js
@@ -1,32 +1,99 @@
-import * as React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
-import Home from './src/containers/home';
-import details from './src/components/details';
-import details from './src/containers/details';
-import {createStackNavigator, createAppContainer} from 'react-navigation-stack';
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import 'react-native-gesture-handler';
+import Banner from './src/container/Banner';
+import RecentlyPlayed from './src/container/RecentlyPlayed';
+import FeaturedEvent from './src/container/FeaturedEvents';
+import Fantasy from './src/container/Fantasy';
+import ChallengeToWin from './src/container/ChallengeToWin';
+import AllGames from './src/container/AllGames';
 
-const rootStack = createStackNavigator(
-  {
-    home: {screen: Home},
-    details: {screen: details},
-  },
-  {initialRouteName: 'home'},
-);
 
-const AppContainer = createAppContainer(rootStack);
+
+const bannerArray = [
+  'banner',
+  'RecentlyPlayed',
+  'FeaturedEvent',
+  'Fantasy',
+  'ChallengeToWin',
+  'AllGames',
+];
 
 export default class App extends React.Component {
   render() {
     return (
-      <View
-        style={{
-          backgroundColor: 'yellow',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text>hello mike</Text>
-        <AppContainer />
-      </View>
+        <View>
+          <View
+            style={{
+              height: 80,
+              backgroundColor: 'red',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{fontSize: 30, fontWeight: 'bold'}}>HEADER</Text>
+          </View>
+         
+          <FlatList
+            style={{backgroundColor: 'blue'}}
+            data={bannerArray}
+            renderItem={({item}) => {
+              switch (item) {
+                case 'banner':
+                  return <Banner />;
+                case 'RecentlyPlayed':
+                  return <RecentlyPlayed />;
+                case 'FeaturedEvent':
+                  return <FeaturedEvent />;
+                case 'Fantasy':
+                  return <Fantasy />;
+                case 'ChallengeToWin':
+                  return <ChallengeToWin />;
+                case 'AllGames':
+                  return <AllGames />;
+              }
+            }}
+            keyExtractor={(item, index) => index}
+          />
+          {/* <ScrollView style={{backgroundColor: 'blue'}}>
+          <Banner />
+          <RecentlyPlayed />
+          <FeaturedEvent />
+          <Fantasy />
+          <ChallengeToWin />
+          <AllGames />
+        </ScrollView> */}
+          <View style={styles.fab}>
+            <TouchableOpacity>
+              <Text style={{color: '#fff', fontSize: 12}}>More Games ▼</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fab: {
+    alignSelf: 'center',
+    height: 40,
+    width: 100,
+    borderRadius: 30,
+    position: 'absolute',
+    bottom: '15%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    borderWidth: 2,
+  },
+});
